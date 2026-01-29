7fcd6b1c435580f3d02a35887290de030c18edff
TimeTREE98 <timetree@kakao.com>
2026-01-30 01:07:34 +0900

    Updated adapter.ts to handle cookies using tough-cookie's CookieJar, allowing for cookie persistence in requests.

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,12 @@
 import 'axios';
 
+import type { CookieJar } from 'tough-cookie';
+
 import { Profile } from './impersonate';
 
 declare module 'axios' {
   export interface AxiosRequestConfig {
     impersonate?: Profile;
+    jar?: CookieJar;
   }
 }
